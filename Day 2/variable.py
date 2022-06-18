acee14f3e9aa143954cb801a41dd2d41eb10ee9d
PanchalPalak <98537429+PanchalPalak@users.noreply.github.com>
2022-06-18 22:46:58 +0530

    Add files via upload

--- Day 2/variable.py
+++ Day 2/variable.py
@@ -1,1 +1,17 @@
+#this is comment part.
+'''this is alternate of comment part.'''
 
+"""This is second alternative of comment part."""
+
+#variable:
+a=5
+b=29.7
+c='ndkk'
+print("value of a is ",a)
+print("value of b is ",b)
+print("value of c is ",c)
+
+x=y=z=562
+print("x is",x)
+print("y is",y)
+print("z is",z)
