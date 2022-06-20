9388cbf1b52f8e50682d5ece08f07cb960f46cd9
PanchalPalak <98537429+PanchalPalak@users.noreply.github.com>
2022-06-20 16:17:04 +0530

    Add files via upload

--- Day 2/datatype.py
+++ Day 2/datatype.py
@@ -3,3 +3,33 @@ print(type(a))
 c='sbjj'
 print(type(c))
 print(isinstance(24.5,int))
+
+#string Datatype:
+a='python-djano internship'
+print(a[0:])
+'''slicing'''
+print(a[1:10])
+'''here does not include 10th index.'''
+
+print(a[1:15:2])
+'''here string trace with jumping 1 words.'''
+
+print(a[::-1])
+'''print reverse string'''
+
+print(a[::-2])
+'''print reverse string with jumping 1 word.'''
+
+
+#list datatype:
+lst=[1.45,4,57,'jiny',28]
+print(lst[3:])
+
+#tuple datatype:
+tpl=('hbha',15,63,25.33)
+print(tpl)
+
+#dictonary datatype:
+d={'a':'orange','b':'graps','c':'kiwi'}
+print(type(d))
+print(d['a'])
